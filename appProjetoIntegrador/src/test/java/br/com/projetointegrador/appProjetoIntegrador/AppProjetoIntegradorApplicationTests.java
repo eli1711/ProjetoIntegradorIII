99e34b1363836d25fc164bc5d1d6b845cf91c9ac
unknown <elihofnimariano@gmail.com>
2025-03-11 19:36:44 -0300

    SpringBootDoProjeto

---
+++ appProjetoIntegrador/src/test/java/br/com/projetointegrador/appProjetoIntegrador/AppProjetoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetointegrador.appProjetoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProjetoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
